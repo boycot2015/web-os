6f0308950d30546e5c750d29b16b52fea723daee
zhangchunhui@yunzhonghe.com <boycot2017@163.com>
2024-07-01 17:06:56 +0800

    更新依赖包

--- svelte.config.js
+++ svelte.config.js
@@ -11,16 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+		// adapter: adapter({
             // images: {
             //     sizes: [640, 828, 1200, 1920, 3840],
             //     formats: ['image/avif', 'image/webp'],
             //     minimumCacheTTL: 300,
             //     domains: ['example-app.vercel.app'],
             // },
-            runtime: 'nodejs18.x',
-			fallback: '200.html' // may differ from host to host
-		})
+        //     runtime: 'nodejs18.x',
+		// 	fallback: '200.html' // may differ from host to host
+		// })
+        adapter: adapter()
 	},
     preprocess: preprocess({
         less: {
